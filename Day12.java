432a5124e5af5f12e7a2e1da3c56f27a387c4bfa
Avnish Patel <er.avnishpatel023@gmail.com>
2025-05-11 10:27:30 +0530

    Day 12 - 1550. Three Consecutive Odds

---
+++ Day12.java
@@ -0,0 +1,18 @@
+// 1550. Three Consecutive Odds
+
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int cnt = 0;
+        for (int x : arr) {
+            if (x % 2 == 1) {
+                if (++cnt == 3) {
+                    return true;
+                }
+            } else {
+                cnt = 0;
+            }
+        }
+        return false;
+    }
+}
